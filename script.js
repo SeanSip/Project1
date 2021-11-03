0be95eb84264457c5b956a41b01db61900c2b794
SeanSip <sean.sipus@gmail.com>
2021-11-03 02:35:52 -0400

    added burger menu with JS functions to home screen

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello, World!");
+
+const hamburgerMenu = document.querySelector('.hamburgerMenu');
+const navMenu = document.querySelector('.navMenu');
+
+hamburgerMenu.addEventListener("click", () => {
+  hamburgerMenu.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
